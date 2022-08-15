7f8e70c9b72725ff228b7674e0b4cdb5239c5584
Hrushikesh Kokardekar <hrushikesh.kok@gmail.com>
2022-08-15 20:13:44 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config({path : "config.env"})
+
+app.get("/", (req, res)=>{
+    res.send("Hello Bloggers")
+});
+
+app.listen(process.env.PORT, ()=>{
+    console.log("APP LISTENING AT PORT 3000");
+})
