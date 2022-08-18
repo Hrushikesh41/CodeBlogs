d3f816a530b86a0b0e01cacc880c319060caf2e4
Hrushikesh Kokardekar <hrushikesh.kok@gmail.com>
2022-08-18 15:49:54 +0530

    Added Responsive Styling to Registration Page

--- client/src/pages/Registration/Register.jsx
+++ client/src/pages/Registration/Register.jsx
@@ -1,51 +1,59 @@
 import react from "react";
 import { IoIosRocket } from "react-icons/io"
 import "./Register.css"
+import bg1 from "../../assets/bg1.jpg"
 
 const Registration = () => {
+
+    const handleSubmit = ()=>{
+        console.log("Registered");
+    }
     return (
         <>
 
             <div className="container">
 
                 <div className="left_inner_container">
-                    
-                    <h1>Welcome <IoIosRocket size={70} color='white'/></h1>
-                    
-                    <h3>You'rer just few steps away from becoming Blogger</h3>
-                    <h3> Register at CodeBlogs and become a professional Tech Blogger</h3>
 
-                    <div className="redirect">
-                        <a href="/login">Already a Blogger</a>
+                    <div className="user_inputs">
+                        <h1>Create an Account</h1>
+
+                        <form className="form_inputs" onSubmit={handleSubmit}>
+                            <input
+                                type="text"
+                                placeholder="Enter Your Name"
+                            />
+
+                            <input
+                                type="email"
+                                placeholder="Enter Your Email"
+                            />
+
+                            <input
+                                type="password"
+                                placeholder="Enter Your Password"
+                            />
+
+                            <input
+                                type="password"
+                                placeholder="Enter Your Password Again"
+                            />
+
+                            <div className="redirect">
+                                <a href="/login">Already a Blogger</a>
+                            </div>
+
+                            <div className="submit_data">
+                                <button type="submit">Register</button>
+                            </div>
+                        </form>
                     </div>
+
+
                 </div>
 
                 <div className="right_inner_container">
-                    <h1>Create an Account</h1>
-
-                    <div className="user_inputs">
-                        <form method="post" className="form_inputs">
-                            <input
-                            type="text"
-                            placeholder="Enter Your Name"
-                            />
-
-                            <input 
-                            type="email"
-                            placeholder="Enter Your Email"
-                            />
-
-                            <input 
-                            type="password"
-                            placeholder="Enter Your Password"
-                            />
-
-                            <input 
-                            type="password"
-                            placeholder="Enter Your Password Again"
-                            />
-                        </form>
-                    </div>
+                    <img src={bg1} alt="Loading...." />
                 </div>
             </div>
         </>
