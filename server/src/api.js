bbc314225561af877654eb01b6dd8f613e5b1bfc
Hrushikesh41 <hrushikesh.kok@gmail.com>
2022-10-24 16:44:06 +0530

    Minor Changes

--- server/src/api.js
+++ server/src/api.js
@@ -19,7 +19,6 @@ app.use(function (req, res, next) {
 app.use(cors({
     origin: ["http://localhost:3000", "http://localhost:5173", "https://server--codeblog-server.netlify.app/"],
     methods: ["GET", "POST"],
-    credentials: true
 }))
 
 
