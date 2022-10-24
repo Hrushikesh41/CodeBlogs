2e284d84c3d722839d5a1d195c56ff1c15422876
Hrushikesh41 <hrushikesh.kok@gmail.com>
2022-10-24 14:48:01 +0530

    Minor Changes

--- client/src/domain.js
+++ client/src/domain.js
@@ -1,5 +1,5 @@
 const url = {
-    base:import.meta.env.VITE_DEVELOPMENT ? "http://localhost:3000/" : "https://codeblog-server.netlify.app/"
+    base:import.meta.env.VITE_DEVELOPMENT ? "https://codeblog-server.netlify.app/" : "http://localhost:3000/"
 }
 
 export default url;
