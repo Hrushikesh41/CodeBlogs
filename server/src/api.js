0f9127590cf23c294446ecb70347b42882dcef67
murtuza <murtuza.creativity@gmail.com>
2022-11-01 16:57:39 +0530

    removed return

--- server/src/api.js
+++ server/src/api.js
@@ -136,7 +136,7 @@ router.post("/logblogger", async (req, res)=>{
     const {email, password} = req.body;
 
     if(!email || !password){
-        return res.status(500).json({error : "Please Enter All Required Feilds"})
+        res.status(500).json({error : "Please Enter All Required Feilds"})
     }else{
         try {
             const fetchUser = await userModels.findOne({email:email});
@@ -146,12 +146,12 @@ router.post("/logblogger", async (req, res)=>{
                 const id = fetchUser._id
                 if(fetchpass){
                     const token = await fetchUser.generateAuthToken();
-                    return res.status(200).json({message : "Login Successful", token, id})
+                    res.status(200).json({message : "Login Successful", token, id})
                 }else{
-                    return res.status(404).json({error : "invalid Password"})
+                    res.status(404).json({error : "invalid Password"})
                 }
             }else{
-                return res.status(404).json({error : "No User Found !!! Please Create an Account"})
+                res.status(404).json({error : "No User Found !!! Please Create an Account"})
             }
         } catch (error) {
             console.log(error);
