ebfff0532072156124a596288edf24bacbe1d133
Hrushikesh41 <hrushikesh.kok@gmail.com>
2022-11-01 11:51:47 +0530

    cors testing

--- server/src/api.js
+++ server/src/api.js
@@ -10,13 +10,17 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 
-app.use(cors())
+// app.use(cors())
 
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
+
+// res.header("Access-Control-Allow-Origin", "*");
+// res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+});
 
 // app.use(cors({
 //     origin: ["http://localhost:3000", "http://localhost:5173", "https://codeblogshrushikesh.netlify.app/", "*"],
