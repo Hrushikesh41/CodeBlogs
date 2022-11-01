03c4985ad0d30fd42e10d55500e744b62eeca038
Hrushikesh41 <hrushikesh.kok@gmail.com>
2022-11-01 11:40:36 +0530

    cors removed

--- server/src/api.js
+++ server/src/api.js
@@ -10,11 +10,11 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 // app.use(cors({
 //     origin: ["http://localhost:3000", "http://localhost:5173", "https://codeblogshrushikesh.netlify.app/", "*"],
