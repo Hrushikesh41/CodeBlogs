b4a947be4a835742040d72d298055e9faa9a3abe
murtuza <murtuza.creativity@gmail.com>
2022-11-11 20:05:37 +0530

    removed conditional checking for base url

--- client/src/domain.js
+++ client/src/domain.js
@@ -1,5 +1,5 @@
 const url = {
-    base:import.meta.env.VITE_DEVELOPMENT ? "http://localhost:3000/" : "https://codeblog-server.netlify.app/" 
+    base: "https://codeblog-server.netlify.app/" 
 }
 
 export default url;
